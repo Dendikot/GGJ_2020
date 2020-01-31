36761ac54415d7c5753c6d8a1a096ad69c9f3f0f
Jan David Wasem <sourcetree@klarnetz.de>
2020-01-31 20:12:31 +0100

    Basic unity project structure

---
+++ gamejam2020/Assets/Scripts/AirConsoleMessenger/AirConsoleMessage.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+
+public class AirConsoleMessage
+{
+    public int fromDeviceId = 0;
+    public int toDeviceId = 0;
+
+    public string type = "";
+    public Dictionary<string, string> payload = null;
+
+    public AirConsoleMessage(int toDeviceId, string type, Dictionary<string, string> payload)
+    {
+        this.fromDeviceId = AirConsole.instance.GetDeviceId();
+        this.toDeviceId = toDeviceId;
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public AirConsoleMessage(int toDeviceId, string type)
+    {
+        this.fromDeviceId = AirConsole.instance.GetDeviceId();
+        this.toDeviceId = toDeviceId;
+        this.type = type;
+        this.payload = new Dictionary<string, string>();
+    }
+
+    public AirConsoleMessage()
+    {
+        this.fromDeviceId = AirConsole.instance.GetDeviceId();
+        this.payload = new Dictionary<string, string>();
+    }
+
+    public void Send()
+    {
+        AirConsoleMessenger.Instance.SendMessage(this);
+    }
+
+    public AirConsoleMessage AddPayload(string key, string value)
+    {
+        this.payload.Add(key, value);
+
+        return this;
+    }
+
+    public AirConsoleMessage AddPayload(string key)
+    {
+        this.AddPayload(key, "");
+        return this;
+    }
+
+    public bool PayloadExists(string key)
+    {
+        if(this.payload.ContainsKey(key))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool PayloadHasValue(string key)
+    {
+        if (this.PayloadExists(key) && !string.IsNullOrEmpty(this.payload[key]))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ gamejam2020/Assets/Scripts/AirConsoleMessenger/AirConsoleMessenger.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleMessenger
+{
+    public const int BroadcastId = -1;
+
+    public delegate void OnMessageHandler(int fromDeviceId, AirConsoleMessage message);
+    public delegate void OnReadyHandler(string joinCode);
+    public delegate void OnConnectHandler(int connectedDeviceId);
+
+    private OnMessageHandler onMessageHandler = null;
+    private OnReadyHandler onReadyHandler = null;
+    private OnConnectHandler onConnectHandler = null;
+
+    private static AirConsoleMessenger instance = null;
+    
+    private AirConsoleMessenger() { }
+
+    ~AirConsoleMessenger()
+    {
+        this.UnregisterEvents();
+    }
+
+    public static AirConsoleMessenger Instance
+    {
+        get
+        {
+            if (AirConsoleMessenger.instance == null)
+            {
+                AirConsoleMessenger.instance = new AirConsoleMessenger();
+            }
+
+            return AirConsoleMessenger.instance;
+        }
+    }
+
+    private void OnMessage(int fromDeviceId, JToken data)
+    {
+        AirConsoleMessage message = data.ToObject<AirConsoleMessage>();
+        this.onMessageHandler(fromDeviceId, message);
+    }
+
+    private void OnConnect(int connectedDeviceId)
+    {
+        //this.CustomDebug("c: " + connectedDeviceId.ToString());
+        this.onConnectHandler(connectedDeviceId);
+    }
+
+    private void OnDisconnect(int disconnectedDeviceId)
+    {
+        //this.CustomDebug("d: " + disconnectedDeviceId.ToString());
+    }
+
+    private void OnReady(string joinCode)
+    {
+        this.ShowDefaultUI(false);
+        this.onReadyHandler(joinCode);
+    }
+
+    public void SendMessage(AirConsoleMessage message)
+    {
+        if(message.toDeviceId < 0)
+        {
+            AirConsole.instance.Broadcast(message);
+        }
+
+        else
+        {
+            AirConsole.instance.Message(message.toDeviceId, message);
+        }
+    }
+
+    public void ShowDefaultUI(bool showDefaultUI)
+    {
+        AirConsole.instance.ShowDefaultUI(showDefaultUI);
+    }
+
+    public void RegisterEvents(OnReadyHandler onReadyHandler, OnConnectHandler onConnectHandler, OnMessageHandler onMessageHandler)
+    {
+        this.onReadyHandler = onReadyHandler;
+        this.onConnectHandler = onConnectHandler;
+        this.onMessageHandler = onMessageHandler;
+
+        AirConsole.instance.onReady += this.OnReady;
+        AirConsole.instance.onConnect += this.OnConnect;
+        AirConsole.instance.onMessage += this.OnMessage;
+        AirConsole.instance.onDisconnect += this.OnDisconnect;
+    }
+
+    public void UnregisterEvents()
+    {
+        this.onReadyHandler = null;
+        this.onConnectHandler = null;
+        this.onMessageHandler = null;
+
+        // unregister events
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onReady -= this.OnReady;
+            AirConsole.instance.onConnect -= this.OnConnect;
+            AirConsole.instance.onMessage -= this.OnMessage;
+            AirConsole.instance.onDisconnect -= this.OnDisconnect;
+        }
+    }
+}
